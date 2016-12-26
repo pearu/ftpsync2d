b6bfca0562c5c3313a09f6d090bc3c773fdf2db6
Kameecoding <andrej.kovac.ggc@gmail.com>
2016-12-26 01:36:37 +0100

    Fixing issue #7 a bit more nicely

--- ftpsync.py
+++ ftpsync.py
@@ -369,7 +369,9 @@ class FtpSession(object):
         if parent!='/':
             self.makedirs(parent, verbose=verbose)
         lst = self.ftp.nlst(parent)
-        name = '/' + name
+        
+        if lst and lst[0].startswith('/'):
+            name = '/' + name
         if name and name not in lst:
             if verbose:
                 sys.stdout.write('<creating directory %r>' % (fullpath))
